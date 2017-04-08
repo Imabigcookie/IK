2a7963986949d7aedd4f3697a7b65ec33e402885
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-04-08 17:35:49 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,13 +3,13 @@
 
 SCENARIO(" Zero ") 
 {
-tree test;
+tree <int> test;
 REQUIRE(root_.test() == nullptr);
 REQUIRE(get_count.test() == 0);	
 }
 SCENARIO(" 1 ")
 {
-  tree test;
+  tree <int> test;
   test.ins_node(7);
   REQUIRE(test.get_count() == 1);
   REQUIRE(test.find_node(7, test.root_())->key == 7);
